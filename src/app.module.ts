36120f593cbb2214005493c6f4bf56f2fbe68745
GustavoZeglan <luizgustavozeglan1@gmail.com>
2025-01-16 20:53:04 -0300

    feat: adding `orm` configuration, endpoint `/status` and test for him

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,16 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { DatabaseModule } from './database/database.module';
 import * as dotenv from "dotenv";
+import { DatabaseService } from "./database/database.service";
 dotenv.config();
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: async () => ({
-        type: "postgres",
-        host: process.env.POSTGRES_HOST,
-        port: Number(process.env.POSTGRES_PORT),
-        username: String(process.env.POSTGRES_USER),
-        password: String(process.env.POSTGRES_PASSWORD),
-        database: process.env.POSTGRES_DB,
-        logging: process.env.DATABASE_LOGGING === "true",
-        migrations: [
-          __dirname + "/infra/typeorm/migrations/**/*{.ts,.js}",
-        ],
-        entities: [__dirname + "/**/*.entity.{js,ts}"],
-        synchronize: false,
-        migrationsRun: true,
-      }),
-    }),
+    DatabaseModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
---
+++ src/database/database.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseService } from './database.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => ({
+        type: "postgres",
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: String(process.env.POSTGRES_USER),
+        password: String(process.env.POSTGRES_PASSWORD),
+        database: process.env.POSTGRES_DB,
+        logging: process.env.DATABASE_LOGGING === "true",
+        migrations: [
+          __dirname + "/infra/typeorm/migrations/**/*{.ts,.js}",
+        ],
+        entities: [__dirname + "/**/*.entity.{js,ts}"],
+        synchronize: false,
+        migrationsRun: true,
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [DatabaseService]
+})
+export class DatabaseModule { }
