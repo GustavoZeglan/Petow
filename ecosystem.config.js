2e958575c3c3b5a7996285225b57381b57479ce5
Luiz Gustavo Zeglan <89813478+GustavoZeglan@users.noreply.github.com>
2025-04-05 10:31:59 -0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "Petow",
+      script: "Petow/dist/src/main.js",
+      watch: true,
+      env: {
+        DATABASE_LOGGING: process.env.DATABASE_LOGGING,
+        JWT_SECRET: process.env.JWT_SECRET,
+        NODE_ENV: process.env.NODE_ENV,
+        POSTGRES_DB: process.env.POSTGRES_DB,
+        POSTGRES_HOST: process.env.POSTGRES_HOST,
+        POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+        POSTGRES_PORT: process.env.POSTGRES_PORT,
+        POSTGRES_USER: process.env.POSTGRES_USER
+      }
+    }
+  ]
+};
