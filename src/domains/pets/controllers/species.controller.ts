fe8c621e5d176b9cb1c58663107ec2468a63ec2d
GustavoZeglan <luizgustavozeglan1@gmail.com>
2025-04-05 10:45:50 -0300

    fix: fixing `query` validation in `species.controller.ts`

--- src/domains/pets/controllers/species.controller.ts
+++ src/domains/pets/controllers/species.controller.ts
@@ -10,13 +10,14 @@ import {
 } from "@nestjs/common";
 import { SpeciesService } from "@pets/services/species.service";
 import { ListSpeciesDTO } from "@pets/dtos/ListSpecieDTO";
+import { JoiPipe } from "nestjs-joi";
 
 @Controller("species")
 export class SpeciesController {
   constructor(private readonly speciesService: SpeciesService) {}
 
   @Get()
-  async findAll(@Req() req: RequestDTO, @Query() query: ListSpeciesDTO) {
+  async findAll(@Req() req: RequestDTO, @Query(JoiPipe) query: ListSpeciesDTO) {
     Logger.log(`User ${req.user.id} is trying to get all species`);
     const species = await this.speciesService.find(query);
     return new HttpResponseDTO(
