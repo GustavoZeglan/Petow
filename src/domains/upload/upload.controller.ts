be9facb044b297f807b9fd082a7c6ca3c031dc1b
GustavoZeglan <lgzeglan@gmail.com>
2025-05-14 21:09:53 -0300

    Adding return of filename

--- src/domains/upload/upload.controller.ts
+++ src/domains/upload/upload.controller.ts
@@ -5,28 +5,45 @@ import {
   UseInterceptors,
   Get,
   Param,
+  HttpStatus,
 } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { Express } from "express";
 import { UploadService } from "./upload.service";
+import { HttpResponseDTO } from "@architecture/dtos/HttpResponseDTO";
 
 @Controller("uploads")
 export class UploadController {
-  constructor(readonly uploadService: UploadService) {}
+  constructor(readonly uploadService: UploadService) { }
 
   @Get("buckets")
-  bucketsList() {
-    return this.uploadService.bucketsList();
+  async bucketsList() {
+    const buckets = this.uploadService.bucketsList();
+    return new HttpResponseDTO(
+      HttpStatus.CREATED,
+      `Buckets retrieved successfully`,
+      buckets,
+    );
   }
 
   @Get("file-url/:name")
-  getFile(@Param("name") name: string) {
-    return this.uploadService.getFile(name);
+  async getFile(@Param("name") name: string) {
+    const fileUrl = await this.uploadService.getFile(name);
+    return new HttpResponseDTO(
+      HttpStatus.CREATED,
+      `File retrieved successfully`,
+      fileUrl,
+    );
   }
 
   @Post()
   @UseInterceptors(FileInterceptor("file"))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    return this.uploadService.uploadFile(file);
+    const obj = await this.uploadService.uploadFile(file);
+    return new HttpResponseDTO(
+      HttpStatus.CREATED,
+      `File Uploaded successfully`,
+      obj,
+    );
   }
 }
--- src/domains/upload/upload.service.ts
+++ src/domains/upload/upload.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, Logger } from "@nestjs/common";
 import { randomUUID } from "crypto";
 import * as Minio from "minio";
 import { InjectMinio } from "@architecture/decorators/minio.decorator";
@@ -8,36 +8,36 @@ import { InjectMinio } from "@architecture/decorators/minio.decorator";
 export class UploadService {
   protected _bucketName = "petowuploads";
 
-  constructor(@InjectMinio() private readonly minioService: Minio.Client) {}
+  constructor(@InjectMinio() private readonly minioService: Minio.Client) { }
 
   async bucketsList() {
     return await this.minioService.listBuckets();
   }
 
   async getFile(filename: string) {
-    return await this.minioService.presignedUrl(
+    const url = await this.minioService.presignedUrl(
       "GET",
       this._bucketName,
       filename,
     );
+    return url;
   }
 
-  uploadFile(file: Express.Multer.File) {
-    return new Promise((resolve, reject) => {
+  async uploadFile(file: Express.Multer.File) {
+    try {
       const filename = `${randomUUID().toString()}-${file.originalname}`;
-      this.minioService.putObject(
+      await this.minioService.putObject(
         this._bucketName,
         filename,
         file.buffer,
         file.size,
-        (error, objInfo) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(objInfo);
-          }
-        },
       );
-    });
+      return filename;
+    } catch (error) {
+      Logger.error(error)
+      throw new InternalServerErrorException(
+        "Failed to upload file",
+      );
+    }
   }
 }
